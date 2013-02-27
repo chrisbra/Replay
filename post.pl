43a9df0baf01719cb95a632e3950ffea7b72715c
Christian Brabandt <cb@256bit.org>
2013-02-27 21:52:44 +0100

    better upload script

--- post.pl
+++ post.pl
@@ -4,13 +4,40 @@ use warnings;
 
 use WWW::Mechanize;
 
-#my $sid=shift @ARGV;
-my $sid=3216;
-my $file=shift @ARGV;
-my $scriptversion=shift @ARGV;
-my $versioncomment=shift @ARGV;
+sub GetPassword() {
+	my $i=0;
+	my @pass;
+	my $passfile="./vim_passfile"; # line1: username, line2: password
+	open(PASS, '<',$passfile) or die "Can't open passwordfile: $passfile\n";
+	while(<PASS>){
+		chomp;
+		$pass[$i++] = $_;
+	}
+	close(PASS);
+	return @pass;
+}
 
-my @userpasswordpair=qw(user password);
+my $sid=3216;
+my $file;
+my @files=glob('*.vmb');
+#my $scriptversion=shift @ARGV;
+my $scriptversion = 0;
+my $versioncomment=shift @ARGV;
+unless ($versioncomment){
+	print "Please enter comment!\n";
+	exit;
+}
+$versioncomment.="\n(automatically uploaded)";
+
+
+my @userpasswordpair = GetPassword();
+for (@files) {
+	my $f = $_ if [ -f $_ ] && $_ =~ /\w+-[^.]+\.(\d+)\.vmb/;
+	if ($1 > $scriptversion) {
+		$scriptversion=$1;
+		$file = $f;
+	}
+}
 
 my $mech=WWW::Mechanize->new(autocheck => 1);
 $mech->get("http://www.vim.org/login.php");
@@ -25,8 +52,7 @@ $mech->get("http://www.vim.org/scripts/script.php?script_id=$sid");
 $mech->follow_link(text => 'upload new version');
 $mech->form_name("script");
 $mech->field(script_file => $file);
-$mech->field(vim_version => 7.2);
+$mech->field(vim_version => 7.3);
 $mech->field(script_version => $scriptversion);
 $mech->field(version_comment => $versioncomment);
 $mech->click_button(value => "upload");
-print $mech->content;
